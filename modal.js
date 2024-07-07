65d994bef9272301f6e2ad2e2b02387717ca0d1e
VampKunal <164723633+VampKunal@users.noreply.github.com>
2024-07-07 15:01:49 +0530

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,6 +1,4 @@
-// JavaScript for navbar functionality
 
-// Example: Toggle navbar for mobile
 const modalTrigger = document.getElementById('modal-trigger');
 const modalCloseBtn = document.getElementById('modal-close');
 const modal = document.getElementById('modal');
